0b3780fc4f702a3549226d269d95884ef65238e8
afadholi1 <afadholi1@gmail.com>
2026-04-17 22:41:07 +0700

    chore: update CORS config with credentials and localhost origin

--- backend/index.js
+++ backend/index.js
@@ -23,7 +23,10 @@ const app = express();
     }
 })();
 
-app.use(cors());
+app.use(cors({
+    credentials: true,
+    origin: 'http://localhost:5173' 
+}));
 app.use(cookieParser());
 app.use(express.json());
 
